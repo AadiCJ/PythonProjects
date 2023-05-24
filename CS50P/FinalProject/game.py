3d621b8bfbdeac5eddd2bd1813c8b7005aeb4037
AadiCJ <acjha990@gmail.com>
2023-05-24 21:58:07 +0530

    Changed game frame
    The game.py frame shows something now. I am committing because I am going to delete all the code I wrote in hopes of an easier solution to come to my mind

--- CS50P/FinalProject/game.py
+++ CS50P/FinalProject/game.py
@@ -61,7 +61,14 @@ def start(playerIn):
     itemsMenu.add_command(label="Sell", command=setSellMode)
     
     
-    statusBar.pack(fill=X, side=BOTTOM, ipady=2)
+    statusBar.grid(column=1, row=3, sticky=(W, E))
+    statusText = StringVar()
+    statusBar.config(textvariable=statusText)
+    statusText.set("Hello")
+    
+    
+    
+    
     
     
     
@@ -69,8 +76,35 @@ def start(playerIn):
     game.geometry(f"{WIDTH}x{HEIGHT}+0+0")
     game.title(f"{player.user}")
     
+    main = Frame(game)
+    main.grid(column=0, row=0, sticky=(N, S, E, W))
+    game.columnconfigure(0, weight=1)
+    game.rowconfigure(0, weight=1)
+    
+
+    clickerFrame = Frame(main, bg="red", width=(WIDTH/3), height=(round(2*HEIGHT/3)))
+    clicker = Button(clickerFrame, command=lambda:print("clicked"), image=money["noteStack"])
+    #clicker.pack()
+    clickerFrame.grid(column=0, row=0, rowspan=2, sticky="NW")
     
     
+    
+    midasFrame = Frame(main, bg="yellow", height=round(HEIGHT/3), width=round(WIDTH/3))
+    midas = Button(midasFrame, command=lambda:print("midas"), image=buyable["midas"])
+    #midas.pack()
+    midasFrame.grid(column=0, row=2, sticky="SW")
+    
+    
+    buyFrames = Frame(main, bg="green", width=(round(2*WIDTH/3)), height=HEIGHT)
+    buyFrames.grid(column=1, row=0, columnspan=2, rowspan=3, sticky=(N, S, E))
+    
+    
+    
+    
+    
+    
+    
+    game.resizable(False, False)
     game.mainloop()
     
     
