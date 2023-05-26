9507e36533a2fdcc2ca63be8aa02aa864fcfc5ec
AadiCJ <acjha990@gmail.com>
2023-05-26 17:04:30 +0530

    Buying changes
    Added the buy buttons, they have hover text. Added two dictionaries, one with names as keys and list as values and one with images as keys and names and values. Next I will work on increase of price when you buy.

--- CS50P/FinalProject/game.py
+++ CS50P/FinalProject/game.py
@@ -26,6 +26,16 @@ buyable = {
     "squirrel": None,
 }
 
+buyableDescriptions = {
+    "dwarf": ["A dwarf to craft money out of rocks for you", 100],
+    "goose": ["A goose that lays very expensive golden eggs", 500000],
+    "midas": ["For every midas you own, your money production doubles", 1000000],
+    "plant": ["A money plant. It's very leaves are money", 500],
+    "printer": ["Prints money.", 100000],
+    "robot": ["Manual labour is quite effective.", 5000],
+    "squirrel": ["Small, cheap and reliable.", 5],
+}
+
 
 HEIGHT = 518
 WIDTH = 900
@@ -43,6 +53,7 @@ def start(playerIn):
     player = playerIn
     getResources()
 
+    global statusBar
     statusBar = Label(game, text="", bd=1, relief=SUNKEN, anchor="e")
 
     menubar = Menu(game)
@@ -60,12 +71,12 @@ def start(playerIn):
     itemsMenu.add_command(label="Sell", command=setSellMode)
 
     statusBar.pack(fill=X, side=BOTTOM, ipady=2)
+    global statusText
     statusText = StringVar()
     statusBar.config(textvariable=statusText)
     statusText.set("Hello, Welcome")
 
     # TODO: Make all the labels and text variables.
-    # TODO: Make the clicker button
     # TODO: Make the actual money making logic
 
     game.geometry(f"{WIDTH}x{HEIGHT}+0+0")
@@ -75,10 +86,12 @@ def start(playerIn):
     main = Frame(game, bg="red")
 
     clickerFrame = Frame(
-        main, bg="blue", width=WIDTH / 3, height=round(2 * (HEIGHT / 3))
+        main, width=WIDTH / 3, height=round(2 * (HEIGHT / 3))
     )
     midasFrame = Frame(main, bg="yellow", width=WIDTH / 3, height=round(HEIGHT / 3))
     buyFrame = Frame(main, bg="green", width=WIDTH * 2 / 3, height=HEIGHT)
+    buttonsBuyFrame = Frame(buyFrame, bg="blue", height=HEIGHT, width=125)
+    
 
     # pack frames
 
@@ -89,13 +102,33 @@ def start(playerIn):
     midasFrame.pack_propagate(0)
     buyFrame.grid(column=1, row=0, columnspan=2, sticky=NE)
     buyFrame.pack_propagate(0)
+    buttonsBuyFrame.pack(side="right")
+    buttonsBuyFrame.pack_propagate(0)
 
     # Widgets
     clicker = Button(clickerFrame, image=money["noteStack"], command=clicked)
+    
+    
+    buyButtons = {
+        Button(buttonsBuyFrame): "squirrel",
+        Button(buttonsBuyFrame): "dwarf",
+        Button(buttonsBuyFrame): "plant",
+        Button(buttonsBuyFrame): "robot",
+        Button(buttonsBuyFrame): "printer",
+        Button(buttonsBuyFrame): "goose",
+    }
+    for button in buyButtons:
+        button = Button(buttonsBuyFrame, text=buyButtons[button], font=FONT, width=125)
+        button.bind("<Enter>", buttonEnter)
+        button.bind("<Leave>", buttonExit)
+        button.bind("<Button-1>", buyableClicked)
+        button.pack(side=TOP)
+    
 
     # pack widgets
-    clicker.pack()
+    clicker.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+    game.protocol("WM_DELETE_WINDOW", saveAndExit) 
     game.resizable(False, False)
     game.mainloop()
 
@@ -107,9 +140,9 @@ def loading():
     loadMenu = Toplevel(game)
     loadMenu.config(width=WIDTH / 2, height=HEIGHT / 2)
 
-    global errLabel
-    errLabel = Label(loadMenu, font=FONT)
-    errLabel.place(x=69)
+    global label
+    label = Label(loadMenu, font=FONT)
+    label.place(x=69)
 
     global userName
     userName = Entry(loadMenu, font=FONT)
@@ -124,12 +157,15 @@ def callLoad():
     try:
         p = load(user)
     except ValueError:
-        errLabel.config(text="Player not found")
+        label.config(text="Player not found")
         return
 
     if p:
-        errLabel.config(text="Player loaded.\nYou may now close this")
+        label.config(text="Player loaded.")
+        game.destroy()
         start(p)
+        
+    
 
 
 """ all loading methods end here """
@@ -152,9 +188,46 @@ def saveAndExit():
     
 
 def clicked():
+    #TODO: Make money drop down when clicked. use the other two money sprites.
     player.money += 1
     print(player.money)
 
+def buttonEnter(e):
+    text = f"{buyableDescriptions[e.widget.cget('text')][0]}, price: {buyableDescriptions[e.widget.cget('text')][1]}"
+    statusText.set(text)
+
+
+def buttonExit(e):
+    statusText.set("")
+    
+
+def buyableClicked(e):
+    name = e.widget.cget('text')
+    if(buying):
+        price = buyableDescriptions[name][1]
+        if player.money >= price:
+            buy(name)
+            player.money-=price
+                    
+                
+def buy(name):
+    match(name):
+        case "squirrel":
+            player.squirrel+=buyAmount
+        case "dwarf":
+            player.dwarf+=buyAmount
+        case "plant":
+            player.plant+=buyAmount
+        case "robot":
+            player.robot+=buyAmount
+        case "printer":
+            player.printer+=buyAmount
+        case "goose":
+            player.goose+=buyAmount
+        case "midas":
+            player.midas*=2
+            
+    
 
 """all game methods end here"""
 
@@ -176,4 +249,7 @@ def getResources():
 
 
 if __name__ == "__main__":
-    start(load("new player"))
+    try:
+        start(load("new player"))
+    except ValueError:
+        start(Player("new player"))
